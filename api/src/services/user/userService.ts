3cdfe958aa2111f7b96c977ada9abddd406e960d
lioarce01 <lioarce1@gmail.com>
2024-11-09 20:08:22 -0300

    implementation/ disable user method

--- api/src/services/user/userService.ts
+++ api/src/services/user/userService.ts
@@ -1,19 +1,23 @@
+import { User, UserStatus } from "@prisma/client";
 import prisma from "../../config/config";
 
 class UserService {
+  //GET USERS
   async getAllUsers() {
     const users = await prisma.user.findMany();
 
     return users;
   }
 
+  //GET USER BY ID
   async getUserById(id: string) {
     const user = await prisma.user.findUnique({ where: { id } });
 
     return user;
   }
 
-  async createUser(user: any) {
+  //CREATE USER
+  async createUser(user: User) {
     const newUser = await prisma.user.create({
       data: user,
     });
@@ -21,16 +25,30 @@ class UserService {
     return newUser;
   }
 
-  async updateUser(id: string, user: any) {
+  //UPDATE USER
+  async updateUser(id: string, user: User) {
     const updatedUser = await prisma.user.update({ where: { id }, data: user });
 
     return updatedUser;
   }
 
+  //DELETE USER
   async deleteUser(id: string) {
     const deletedUser = await prisma.user.delete({ where: { id } });
 
     return deletedUser;
+  }
+
+  //DISABLE USER
+  async disableUser(id: string) {
+    const disabledUser = await prisma.user.update({
+      where: { id },
+      data: {
+        status: UserStatus.DISABLED,
+      },
+    });
+
+    return disabledUser;
   }
 }
 
